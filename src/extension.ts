05cff7ed0bb31441c8385d35a8ae06d17409233b
YuruiHong <yuruihong02@outlook.com>
2024-02-21 00:31:14 +0800

    refactor: combine string creation into single step

--- src/extension.ts
+++ src/extension.ts
@@ -159,11 +159,9 @@ export function activate(context: vscode.ExtensionContext) {
         text = text.replace(/`[^`\n]+`/g, '')
         text = text.replace(/<!--[\s\S]+?-->/g, '')
         const ENV_NAMES = ['align*', 'align', 'equation*', 'equation', 'gather*', 'gather', 'multline*', 'multline', 'flalign*', 'flalign', 'eqnarray*', 'eqnarray', 'xalignat', 'xalignat*']
-        let string = String.raw`/(\\\[[^\$]*?\\\])|(\\\([^\$]*?\\\))|(\$\$[^\$]+\$\$)|(\$[^\$]+?\$)`
-        for (let env of ENV_NAMES) {
-            const raw_env = String.raw`${env.replace(/\*/g, '\\*')}`
-            string += String.raw`|(\\begin\{${raw_env}\}[^\$]*?\\end\{${raw_env}\})`
-        }
+        const raw_envs = ENV_NAMES.map(env => String.raw`${env.replace(/\*/g, '\\*')}`);
+        const env_regex = raw_envs.map(raw_env => String.raw`(\\begin\{${raw_env}\}[^\$]*?\\end\{${raw_env}\})`).join('|');
+        const string = String.raw`/(\\\[[^\$]*?\\\])|(\\\([^\$]*?\\\))|(\$\$[^\$]+\$\$)|(\$[^\$]+?\$)|${env_regex}`;
         text = text.replace(RegExp(string, 'g'), '')
         if (text.indexOf('$') == -1 && text.indexOf('\\(') == -1 && text.indexOf('\\[') == -1) {
             find: {
